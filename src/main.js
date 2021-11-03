cb063d3e33eb6b9a74bef8f572e59a636a2dbf19
ryowuandjanet <ryowu0329@gmail.com>
2021-11-04 01:00:43 +0800

    Vue3+Firebae9 Initial

--- src/main.js
+++ src/main.js
@@ -3,4 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZubFSpDf2xGdu5xg8OK7QOacPqU7GAwY",
+  authDomain: "version9-6695f.firebaseapp.com",
+  projectId: "version9-6695f",
+  storageBucket: "version9-6695f.appspot.com",
+  messagingSenderId: "85299313211",
+  appId: "1:85299313211:web:97dd1cba31550d712dfa1e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }
 createApp(App).use(store).use(router).mount('#app')
